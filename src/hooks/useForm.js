3b4d56cbd274a81ea81e1792ca92b023389c3fb6
Sushil <grayson.9999@gmail.com>
2025-07-11 20:21:31 +1000

    adding custom hook useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const handleOnChange = ({ e, form, setForm }) => {
+  const { name, value } = e.target;
+  setForm({
+    ...form,
+    [name]: value,
+  });
+};
+const useForm = (initialState) => {
+  const [form, setForm] = useState(initialState);
+
+  return {
+    form,
+    setForm,
+    handleOnChange: (e) => handleOnChange({ e, form, setForm }),
+  };
+};
+
+export default useForm;
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -3,14 +3,27 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { CustomInput } from "../../components/customInput/CustomInput";
 import { signUpInputs } from "../../assets/customInputs/userSignUpInput";
+import useForm from "../../hooks/useForm";
+
+const initialState = {};
 const SignUpPage = () => {
+  const { form, setForm, handleOnChange } = useForm(initialState);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div className="d-flex justify-content-center">
-      <Form style={{ width: "450px" }} className="card p-5 mt-5 mb-5 shadow-lg">
+      <Form
+        style={{ width: "450px" }}
+        className="card p-5 mt-5 mb-5 shadow-lg"
+        onSubmit={handleOnSubmit}
+      >
         <h1>Join our Library Community!</h1>
         <hr />
         {signUpInputs.map((input, index) => (
-          <CustomInput key={input.name} {...input} />
+          <CustomInput key={input.name} {...input} onChange={handleOnChange} />
         ))}
         <Button variant="primary" type="submit">
           Submit
